7544b4c2a6a68630788607a3ded4e3f8476150f0
Alex Root-Roatch <arootroatch@gmail.com>
2024-11-10 13:08:29 -0600

    init commit, cli module

---
+++ lib/cli.rb
@@ -0,0 +1,39 @@
+module CLI
+
+  $end_states = {
+    :x_wins => "X wins!",
+    :o_wins => "O wins!",
+    :tie => "It's a tie!"
+  }
+
+  def self.print_board(board)
+    formatted = board.map { |e| e.to_s.capitalize }
+    puts "#{formatted[0]} #{formatted[1]} #{formatted[2]}"
+    puts "#{formatted[3]} #{formatted[4]} #{formatted[5]}"
+    puts "#{formatted[6]} #{formatted[7]} #{formatted[8]}"
+  end
+
+  def self.display_turn(token)
+    puts token.to_s.capitalize + "'s turn!"
+  end
+
+  def self.prompt_move(board)
+    puts "Please enter your move (type a number 0-#{board.last} and press 'Enter'):"
+  end
+
+  def self.get_user_input(max)
+    input = gets.chomp
+    parsed = Integer(input, exception: false)
+    if parsed and parsed <= max
+      parsed
+    else
+      puts "Oops! Please enter a number from 0 to 8:"
+      get_user_input(max)
+    end
+  end
+
+  def self.print_result(state)
+    puts $end_states[state]
+  end
+
+end
---
+++ spec/cli_spec.rb
@@ -0,0 +1,73 @@
+require "cli"
+
+describe CLI do
+  it "prints a blank 3x3 board" do
+    board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    expect { CLI.print_board(board) }
+      .to output("0 1 2\n3 4 5\n6 7 8\n").to_stdout_from_any_process
+  end
+
+  it "prints a 3x3 board with tokens" do
+    board = [0, :x, 2, :o, :o, 5, :x, 7, 8]
+    expect { CLI.print_board(board) }
+      .to output("0 X 2\nO O 5\nX 7 8\n").to_stdout_from_any_process
+  end
+
+  it "says whose turn it is" do
+    expect { CLI.display_turn(:x) }
+      .to output("X's turn!\n").to_stdout_from_any_process
+    expect { CLI.display_turn(:o) }
+      .to output("O's turn!\n").to_stdout_from_any_process
+  end
+
+  it "prompts user to input move" do
+    board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    expect { CLI.prompt_move(board) }
+      .to output("Please enter your move (type a number 0-8 and press 'Enter'):\n").to_stdout_from_any_process
+    board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    expect { CLI.prompt_move(board) }
+      .to output("Please enter your move (type a number 0-15 and press 'Enter'):\n").to_stdout_from_any_process
+  end
+
+  context "get user input" do
+    it "only accepts numbers" do
+      allow_any_instance_of(Kernel)
+        .to receive(:gets).and_return("blah", "1")
+      expect {CLI.get_user_input(8)}
+        .to output("Oops! Please enter a number from 0 to 8:\n").to_stdout_from_any_process
+      expect(CLI.get_user_input(8)).to eq(1)
+    end
+
+    it "only accepts numbers up to the given maximum - single digit" do
+      allow_any_instance_of(Kernel)
+        .to receive(:gets).and_return("9", "5")
+      expect {CLI.get_user_input(8)}
+        .to output("Oops! Please enter a number from 0 to 8:\n").to_stdout_from_any_process
+      expect(CLI.get_user_input(8)).to eq(5)
+    end
+
+    it "only accepts numbers up to the given maximum - double digit" do
+      allow_any_instance_of(Kernel)
+        .to receive(:gets).and_return("17", "0")
+      expect {CLI.get_user_input(15)}
+        .to output("Oops! Please enter a number from 0 to 8:\n").to_stdout_from_any_process
+      expect(CLI.get_user_input(8)).to eq(0)
+    end
+
+    it "accounts for leading 0s" do
+      allow_any_instance_of(Kernel)
+        .to receive(:gets).and_return("05")
+      expect(CLI.get_user_input(8)).to eq(5)
+    end
+  end
+
+  it "displays the result of the game" do
+    expect { CLI.print_result(:x_wins) }
+      .to output("X wins!\n").to_stdout_from_any_process
+    expect { CLI.print_result(:o_wins) }
+      .to output("O wins!\n").to_stdout_from_any_process
+    expect { CLI.print_result(:tie) }
+      .to output("It's a tie!\n").to_stdout_from_any_process
+  end
+
+end
